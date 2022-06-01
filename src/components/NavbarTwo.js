6d75537459e20f8d359c87d26dea6d55031c3633
zaki <zaka.abahri@gmail.com>
2022-06-01 07:03:28 +0000

    add custom logo

--- src/components/NavbarTwo.js
+++ src/components/NavbarTwo.js
@@ -1,12 +1,16 @@
+import Logo from '../assets/img/logo.svg'
+
 const NavBarTwo = () => {
   return (
     <header class=" bg-neutral-900 py-1">
       <span class="flex items-center justify-between px-4 py-2 container mx-auto">
-        <img
+        {/*  <img
           src="https://cdn.hashnode.com/res/hashnode/image/upload/v1643004937711/k3NMskkSn.png"
           width="50"
           alt="Daily Dev Tips Logo"
-        />
+        /> */}
+        <img src={Logo} width="75" alt="default" class="bg-neutral-900" />
+
         <strong class="text-white tracking-widest text-xl">
           ZACK'S WEBSITE
         </strong>
