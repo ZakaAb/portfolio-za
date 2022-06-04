50a47f28ccd92fca19ec7fbc076c63055ffcc997
zaki <zaka.abahri@gmail.com>
2022-06-04 23:43:15 +0000

    delete unused

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Footer from './components/Footer'
 import GetInTouch from './components/GetInTouch'
 import Main from './components/Main'
-import Navbar from './components/Navbar'
 import NavBarTwo from './components/NavbarTwo'
 import Skills from './components/Skills'
 import Technologies from './components/Technologies'
@@ -9,7 +8,6 @@ import Technologies from './components/Technologies'
 function App() {
   return (
     <div className="App font-montserrat">
-      {/* <Navbar /> */}
       <NavBarTwo />
       <Main />
       <Skills />
