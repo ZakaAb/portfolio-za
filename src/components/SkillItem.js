fa625080c210545af95ae5a03528aaa2c10a015a
zaki <zaka.abahri@gmail.com>
2022-06-04 16:50:45 +0000

    refactor skills section

---
+++ src/components/SkillItem.js
@@ -0,0 +1,13 @@
+const SkillItem = ({ title, text, icon: Icon }) => {
+  return (
+    <div class="flex flex-col items-center justify-center text-center md:w-1/3 px-5">
+      <Icon class="text-6xl text-red-600" />
+      <h1 class="text-2xl uppercase font-semibold text-red-600 py-4">
+        {title}
+      </h1>
+      <p class="slate-800">{text}</p>
+    </div>
+  )
+}
+
+export default SkillItem
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,38 +1,41 @@
 import { MdOutlineLocalPolice } from 'react-icons/md'
 import { MdFlutterDash } from 'react-icons/md'
 import { MdOutlineBakeryDining } from 'react-icons/md'
+import SkillItem from './SkillItem'
+
+const data = [
+  {
+    id: 1,
+    icon: MdOutlineLocalPolice,
+    title: 'Software Engineering',
+    text: 'Staying ahead of the technology curve, we cover front-end, backend, and full-stack development. Get an advanced and easily-maintainable product that grants high responsiveness and a smooth user journey.',
+  },
+  {
+    id: 2,
+    icon: MdFlutterDash,
+    title: 'DevOps',
+    text: 'Optimize your project infrastructure to make it more efficient and budget-friendly while avoiding security glitches. We’ll take care of building the environment that allows scalability, handles traffic loads, and operates like clockwork.',
+  },
+  {
+    id: 3,
+    icon: MdOutlineBakeryDining,
+    title: 'Project Management',
+    text: 'We’ll accomplish your team with the first-class product owner who will prioritize the backlog for engineers and take care of customer satisfaction by creating user stories. With the help of the project manager.',
+  },
+]
 
 function Skills() {
   return (
     <section class="bg-slate-100 flex items-center">
       <div class="container mx-auto flex flex-col md:justify-between md:flex-row py-24 md:space-x-10 space-y-10 md:space-y-0 items-center">
-        <div class="flex flex-col items-center justify-center text-center md:w-1/3 px-5">
-          <MdOutlineLocalPolice class="text-6xl text-red-600" />
-          <h1 class="text-2xl uppercase font-semibold text-red-600 py-4">
-          Software Engineering
-          </h1>
-          <p class="slate-800">
-          Staying ahead of the technology curve, we cover front-end, backend, and full-stack development. Get an advanced and easily-maintainable product that grants high responsiveness and a smooth user journey.
-          </p>
-        </div>
-        <div class="flex flex-col items-center justify-center text-center md:w-1/3 px-5">
-          <MdFlutterDash class="text-6xl text-red-600" />
-          <h1 class="text-2xl uppercase font-semibold text-red-600 py-4">
-            DevOps
-          </h1>
-          <p class="slate-800">
-          Optimize your project infrastructure to make it more efficient and budget-friendly while avoiding security glitches. We’ll take care of building the environment that allows scalability, handles traffic loads, and operates like clockwork.
-          </p>
-        </div>
-        <div class="flex flex-col items-center justify-center text-center md:w-1/3 px-5">
-          <MdOutlineBakeryDining class="text-6xl text-red-600" />
-          <h1 class="text-2xl uppercase font-semibold text-red-600 py-4">
-          Project Management
-          </h1>
-          <p class="slate-800">
-          We’ll accomplish your team with the first-class product owner who will prioritize the backlog for engineers and take care of customer satisfaction by creating user stories. With the help of the project manager.
-          </p>
-        </div>
+        {data.map((item) => (
+          <SkillItem
+            key={item.id}
+            icon={item.icon}
+            title={item.title}
+            text={item.text}
+          />
+        ))}
       </div>
     </section>
   )
