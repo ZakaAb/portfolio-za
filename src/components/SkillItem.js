c79bb0b148eff8dd642cff1ca52a59129e0f80a0
zaki <zaka.abahri@gmail.com>
2022-06-04 16:53:06 +0000

    enhance..

--- src/components/SkillItem.js
+++ src/components/SkillItem.js
@@ -1,13 +1,9 @@
-const SkillItem = ({ title, text, icon: Icon }) => {
-  return (
-    <div class="flex flex-col items-center justify-center text-center md:w-1/3 px-5">
-      <Icon class="text-6xl text-red-600" />
-      <h1 class="text-2xl uppercase font-semibold text-red-600 py-4">
-        {title}
-      </h1>
-      <p class="slate-800">{text}</p>
-    </div>
-  )
-}
+const SkillItem = ({ title, text, icon: Icon }) => (
+  <div class="flex flex-col items-center justify-center text-center md:w-1/3 px-5">
+    <Icon class="text-6xl text-red-600" />
+    <h1 class="text-2xl uppercase font-semibold text-red-600 py-4">{title}</h1>
+    <p class="slate-800">{text}</p>
+  </div>
+)
 
 export default SkillItem
