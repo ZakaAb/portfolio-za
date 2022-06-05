c116201bf137fc2dcace9e291ca8cb22325d8c5a
zaki <zaka.abahri@gmail.com>
2022-06-05 00:09:40 +0000

    add hamburger menu

--- src/components/NavbarTwo.js
+++ src/components/NavbarTwo.js
@@ -7,9 +7,15 @@ const NavBarTwo = () => {
       block: 'start',
     })
   }
+
+  const toggleMenu = () => {
+    document.getElementById('menu-btn').classList.toggle('open')
+    document.getElementById('menu').classList.toggle('flex')
+    document.getElementById('menu').classList.toggle('hidden')
+  }
   return (
     <header class=" bg-neutral-900">
-      <div class="flex items-center sm:justify-between justify-center px-10 py-4 container mx-auto">
+      <div class="flex items-center justify-between px-10 py-4 container mx-auto">
         {/*  <img
           src="https://cdn.hashnode.com/res/hashnode/image/upload/v1643004937711/k3NMskkSn.png"
           width="50"
@@ -40,7 +46,42 @@ const NavBarTwo = () => {
             Contact
           </a>
         </nav>
+
+        <button
+          id="menu-btn"
+          class="block hamburger sm:hidden focus:outline-none"
+          onClick={toggleMenu}
+        >
+          <span class="hamburger-top"></span>
+          <span class="hamburger-middle"></span>
+          <span class="hamburger-bottom"></span>
+        </button>
       </div>
+      <nav class="sm:hidden">
+        <div
+          id="menu"
+          class="absolute  flex-col items-center hidden z-[999] self-end pb-8 space-y-6 font-bold bg-neutral-900 text-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md"
+        >
+          <a
+            onClick={() => handleClick('#skills')}
+            class="hover:text-red-600 cursor-pointer uppercase"
+          >
+            Skills
+          </a>
+          <a
+            onClick={() => handleClick('#technologies')}
+            class="hover:text-red-600 cursor-pointer uppercase"
+          >
+            Technologies
+          </a>
+          <a
+            onClick={() => handleClick('#contact')}
+            class="hover:text-red-600 cursor-pointer uppercase"
+          >
+            Contact
+          </a>
+        </div>
+      </nav>
     </header>
   )
 }
